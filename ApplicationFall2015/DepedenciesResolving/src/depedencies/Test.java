66b66f4d65de65d094ae97649cc9d0a633369b5a
NikolayBonev <niki9442@abv.bg>
2015-11-01 22:57:46 +0200

    ApplicationFall2015 Hack Bulgaria

---
+++ ApplicationFall2015/DepedenciesResolving/src/depedencies/Test.java
@@ -0,0 +1,12 @@
+package depedencies;
+
+public class Test {
+
+	public static void main(String[] args) {
+		DepedenciesResolving test = new DepedenciesResolving();
+		
+		test.installer();
+
+	}
+
+}
